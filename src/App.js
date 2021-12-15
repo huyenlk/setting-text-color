15a39abc3c3b52a35beded723b5e9804cb8ae2b1
Wuyen <taokoquantammaycungdungquantam@gmail.com>
2021-12-15 17:15:42 +0700

    lesson05-handle-events

--- src/App.js
+++ src/App.js
@@ -1,52 +1,78 @@
 import './App.css';
 import { Component } from 'react';
-import Product from './Components/Product';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Products: [
+        {
+          id: 1,
+          name: 'apple ip 6s plus 16GB',
+          price: 15000000,
+          images: 'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_6s_plus_2.jpg',
+          status: true
+        },
+        {
+          id: 2,
+          name: 'ss galaxy s7',
+          price: 15000000,
+          images: 'https://www.viettablet.com/images/detailed/26/samsung-galaxy-s7-edge-viettablet.png',
+          status: true
+        },
+        {
+          id: 3,
+          name: 'apple ip 6s plus 16GB',
+          price: 15000000,
+          images: 'https://cdn.tgdd.vn/Products/Images/42/89692/oppo-f1s-mau-den-org-den.png',
+          status: true
+        }
+      ],
+      isActive: true
+    };
+  }
+  onSetState = () => {
+    if (this.state.isActive === true) {
+      this.setState({
+        isActive: false
+      });
+    } else {
+      this.setState({
+        isActive: true
+      });
+    }
+  }
+
   render() {
-    var Products = [
-      {
-        id: 1,
-        name: 'apple ip 6s plus 16GB',
-        price: 15000000,
-        images: 'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_6s_plus_2.jpg',
-        status: true
-      },
-      {
-        id: 2,
-        name: 'ss galaxy s7',
-        price: 15000000,
-        images: 'https://www.viettablet.com/images/detailed/26/samsung-galaxy-s7-edge-viettablet.png',
-        status: true
-      },
-      {
-        id: 3,
-        name: 'apple ip 6s plus 16GB',
-        price: 15000000,
-        images: 'https://cdn.tgdd.vn/Products/Images/42/89692/oppo-f1s-mau-den-org-den.png',
-        status: false
-      }
-    ];
     return (
       <div>
         <div className="btn-group" role="group" aria-label="Basic example">
-          <button type="button" className="btn btn-dark">Props</button>
+          <button type="button" className="btn btn-dark">State</button>
         </div>
         <div className='container'>
           <div className='row'>
-            {Products.map((Products, index) => {
-              let result = ''
-              if (Products.status) {
-                return result = <Product
-                  key={Products.id}
-                  price={Products.price}
-                  images={Products.images}
-                >
-                  {Products.name}
-                </Product>
-              }
-              return result;
-            })};
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">STT</th>
+                  <th scope="col">Ten san pham</th>
+                  <th scope="col">Gia</th>
+                </tr>
+              </thead>
+              {this.state.isActive === true ? <tbody>
+                {this.state.Products.map((Products, index) => {
+                  return <tr key={index}>
+                    <th scope="row">{Products.id}</th>
+                    <td>{Products.name}</td>
+                    <td>{Products.price}</td>
+                  </tr>
+                })}
+              </tbody> : ''}
+
+            </table>
+            <button type="button" className="btn btn-warning" onClick={this.onSetState}>
+              Active: {this.state.isActive === true ? 'true' : 'false'}
+            </button>
           </div>
         </div>
       </div>
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,6 +1,16 @@
 import { Component } from 'react';
 
 class Product extends Component {
+
+  /*constructor(props) {
+    super(props);
+    this.onAddToCard =this.onAddToCard.bind(this);
+  }*/
+
+  onAddToCard = () => {
+    alert(this.props.children+'-'+this.props.price);
+  }
+
   render() {
     return (
       <div className='col-md-4 col-lg-4 col-sm-4 col-xs-4'>
@@ -9,7 +19,7 @@ class Product extends Component {
           <div className="card-body">
             <h5 className="card-title">{this.props.children}</h5>
             <p className="card-text">{this.props.price} VND</p>
-            <button className="btn btn-primary">Mua ngay</button>
+            <button className="btn btn-primary" onClick={this.onAddToCard}>Mua ngay</button>
           </div>
         </div>
       </div>
