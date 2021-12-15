805440c8d12506f927f8736c12087b2770765dde
Wuyen <taokoquantammaycungdungquantam@gmail.com>
2021-12-15 17:21:32 +0700

    lesson08-form

--- src/App.js
+++ src/App.js
@@ -2,81 +2,91 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      Products: [
-        {
-          id: 1,
-          name: 'apple ip 6s plus 16GB',
-          price: 15000000,
-          images: 'https://cdn.cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone_6s_plus_2.jpg',
-          status: true
-        },
-        {
-          id: 2,
-          name: 'ss galaxy s7',
-          price: 15000000,
-          images: 'https://www.viettablet.com/images/detailed/26/samsung-galaxy-s7-edge-viettablet.png',
-          status: true
-        },
-        {
-          id: 3,
-          name: 'apple ip 6s plus 16GB',
-          price: 15000000,
-          images: 'https://cdn.tgdd.vn/Products/Images/42/89692/oppo-f1s-mau-den-org-den.png',
-          status: true
-        }
-      ],
-      isActive: true
+      txtUsername: '',
+      txtPassword: '',
+      txtDesc: '',
+      txtGender: '',
+      rdLang: '',
+      chkbStatus: ''
     };
   }
-  onSetState = () => {
-    if (this.state.isActive === true) {
-      this.setState({
-        isActive: false
-      });
-    } else {
-      this.setState({
-        isActive: true
-      });
-    }
+
+  onHandleChange = (event) => {
+    var target = event.target;
+    var name = target.name;
+    var value = target.name==='chkbStatus'? target.checked: target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onHandleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
   }
 
   render() {
     return (
-      <div>
-        <div className="btn-group" role="group" aria-label="Basic example">
-          <button type="button" className="btn btn-dark">State</button>
-        </div>
-        <div className='container'>
-          <div className='row'>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">STT</th>
-                  <th scope="col">Ten san pham</th>
-                  <th scope="col">Gia</th>
-                </tr>
-              </thead>
-              {this.state.isActive === true ? <tbody>
-                {this.state.Products.map((Products, index) => {
-                  return <tr key={index}>
-                    <th scope="row">{Products.id}</th>
-                    <td>{Products.name}</td>
-                    <td>{Products.price}</td>
-                  </tr>
-                })}
-              </tbody> : ''}
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-8 col-xs-8 col-lg-8 col-sm-8'>
+            <form onSubmit={this.onHandleSubmit} className="card">
+              <div className="card-header">
+                Form
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Username</h5>
+                <input type='text' name='txtUsername' className='form-control' onChange={this.onHandleChange} />
+                <h5 className="card-title">Password</h5>
+                <input type='text' name='txtPassword' className='form-control' onChange={this.onHandleChange} />
+                <h5 className="card-title">Mo ta</h5>
+                <textarea className="form-control" name='txtDesc' onChange={this.onHandleChange}></textarea>
+                <h5 className="card-title">Mo ta</h5>
+                <select className="form-select" name='txtGender' value={this.state.txtGender} onChange={this.onHandleChange}>
+                  <option value="0">Nu</option>
+                  <option value="1">Nam</option>
+                </select>
+                <h5 className="card-title">Ngon ngu</h5>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" value="en" onChange={this.onHandleChange} name='rdLang' checked={this.state.rdLang === 'en'} />
+                  <label className="form-check-label" >
+                    Tieng Anh
+                  </label>
 
-            </table>
-            <button type="button" className="btn btn-warning" onClick={this.onSetState}>
-              Active: {this.state.isActive === true ? 'true' : 'false'}
-            </button>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="radio" value="vi" onChange={this.onHandleChange} name='rdLang' checked={this.state.rdLang === 'vi'} />
+                  <label className="form-check-label" >
+                    Tieng Viet
+                  </label>
+                </div>
+                <h5 className="card-title">Trang thai</h5>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" name="chkbStatus" onChange={this.onHandleChange} value={true}/>
+                  <label className ="form-check-label">
+                  Dung
+                  </label>
+                </div>
+                <div className="form-check">
+                  <input className="form-check-input" type="checkbox" name="chkbStatus" onChange={this.onHandleChange} value={false}/>
+                  <label className ="form-check-label">
+                  Sai
+                  </label>
+                </div>
+                <button type='submit' className="btn btn-primary">Luu lai</button>
+                &nbsp;
+                <button type='reset' className="btn btn-light">Xoa trang</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
     );
   }
 }
+
 export default App;
